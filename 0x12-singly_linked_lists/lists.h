0580b8c307782d64b7ebbc93cae23f3014f82207
Regina Asiedua <rnaatenkorang@gmail.com>
2022-04-22 23:12:59 +0000

    minor changes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,27 +1,29 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
+/*
+* Desc: header file containinf prototypes and definitions
+*	for all functions and types written in this proj
+*/
+
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * struct list_t - singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ *              for Holberton project
  */
-typedef struct list_t
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_t *next;
+	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
